2014eb85be208d7855b5ce94f17e7b9a9954d61f
argeulimbo <argeusousaptbr@gmail.com>
2024-09-24 09:51:08 -0300

    Implements findAll on SellerDaoJDBC

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -96,8 +96,42 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*, department.Name as DepName "
+					+ "FROM coursejdbc.seller INNER JOIN coursejdbc.department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "ORDER BY Name");
+			
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+			
+			while (rs.next()) {
+				
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				if (dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				Seller obj = instantiateSeller(rs, dep);
+				list.add(obj);
+			}
+			return list;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
@@ -111,7 +145,7 @@ public class SellerDaoJDBC implements SellerDao {
 					+ "FROM coursejdbc.seller INNER JOIN coursejdbc.department "
 					+ "ON seller.DepartmentId = department.Id "
 					+ "WHERE DepartmentId = ?"
-					+ "ORDER BY Name");
+					+ "ORDER BY Name, Id");
 			
 			st.setInt(1, department.getId());
 			rs = st.executeQuery();
